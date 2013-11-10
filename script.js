a856fb00a5088a7db68525571600ab03f7422e72
Evan <brooks.ev@gmail.com>
2013-11-10 13:56:09 -0500

    Default tilt to give some motion to other browsers

--- script.js
+++ script.js
@@ -375,11 +375,13 @@
   }
 
 
-  var tilt = 0, roll = 0;
-  window.addEventListener("deviceorientation", function () {
-      tilt = event.beta;
-      roll = event.gamma;
-  }, true);
+  var tilt = -3, roll = 0;
+  window.addEventListener("deviceorientation", tilt_detect, true);
+
+  function tilt_detect(event) {
+    tilt = event.beta;
+    roll = event.gamma;
+  }
 
   function tiltsense(it) {
     it.vel.x += 0.0001 * roll;
